ab4e8eb50acd9bc5e2fa453f32dc47706531d279
Márcio Simão <masimao@gmail.com>
2015-07-11 17:50:16 -0300

    Update maskbrphone.js
    Removido hífem entre o primeiro e o segundo dígito

--- maskbrphone.js
+++ maskbrphone.js
@@ -26,16 +26,16 @@
 
             var fone = input
                     .replace(/\D/g, '') // Remove tudo o que não for dígito
-                    .substring(0, 9); // Limita a 9 caracteres;
+                    .substring(0, 9); // Limita em 9 caracteres;
 
             var maskedPhone;
 
             if (fone.length <= 8) {
-                // Coloca hífen entre o quarto e o quinto dígitos
+                // Coloca hífen entre o quarto e o quinto dígito
                 maskedPhone = fone.replace(/(\d{4})(\d{1,4})/, '$1-$2');
             } else {
-                // Coloca hífen entre o primeiro e o segundo e o quinto e o sexto dígitos
-                maskedPhone = fone.replace(/(\d{1})(\d{1,4})(\d{1,4})/, '$1-$2-$3');
+                // Coloca hífen entre o quinto e o sexto dígito
+                maskedPhone = fone.replace(/(\d{5})(\d{1,4})/, '$1-$2');
             }
 
             return maskedPhone;
