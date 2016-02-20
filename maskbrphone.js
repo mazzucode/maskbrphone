db38449c4e7e741fc9ecec61882273dcdec30172
Marcio Mazzucato <masimao@gmail.com>
2016-02-20 13:19:56 -0300

    Permite que o usuário configure o comportamento do plugin

--- maskbrphone.js
+++ maskbrphone.js
@@ -1,17 +1,24 @@
 /**
-*  maskbrphone for jQuery, version 1.0.0
-*  (c) 2014 Márcio Simão (https://github.com/masimao)
-*
-*  maskbrphone for jQuery is freely distributable under the terms of an MIT-style license.
-*  For details, see the web site: https://github.com/masimao/maskbrphone
-*/
+ *  maskbrphone for jQuery, version 2.0.0 (https://github.com/masimao/maskbrphone)
+ *  (c) 2014 Márcio Mazzucato (https://github.com/masimao)
+ *
+ *  maskbrphone for jQuery is freely distributable under the terms of an MIT-style license.
+ */
 (function($) {
 
-    $.fn.maskbrphone = function() {
+    $.fn.maskbrphone = function(options) {
 
+    	var defaults = {
+            useDdd            : false,
+            useDddParenthesis : true,
+            dddSeparator      : ' ',
+            numberSeparator   : '-'
+        };
+
+        var settings = $.extend({}, defaults, options);
         var plugin = this;
 
-        var init = function() {
+        var initialize = function() {
 
             return plugin.each( function() {
                 $(this).val(applyMask($(this).val()));
@@ -24,24 +31,48 @@
 
         var applyMask = function(input) {
 
-            var fone = input
-                    .replace(/\D/g, '') // Remove tudo o que não for dígito
-                    .substring(0, 9); // Limita em 9 caracteres;
-
-            var maskedPhone;
-
-            if (fone.length <= 8) {
-                // Coloca hífen entre o quarto e o quinto dígito
-                maskedPhone = fone.replace(/(\d{4})(\d{1,4})/, '$1-$2');
-            } else {
-                // Coloca hífen entre o quinto e o sexto dígito
-                maskedPhone = fone.replace(/(\d{5})(\d{1,4})/, '$1-$2');
+            if ( ! input.length) {
+                return '';
             }
 
-            return maskedPhone;
+            var fone = input.replace(/\D/g, ''); // Remove tudo, menos dígitos
+
+            var ddd,
+                prefix,
+                suffix;
+
+            if ( ! settings.useDdd && input.charAt(0) === '9') {
+                prefix = fone.substring(0, 5);
+                suffix = fone.substring(5, 9);
+            } else if (settings.useDdd && input.charAt(5) === '9') {
+                ddd = fone.substring(0, 2);
+                prefix = fone.substring(2, 7);
+                suffix = fone.substring(7, 11);
+            } else if ( ! settings.useDdd && input.charAt(0) !== '9') {
+                prefix = fone.substring(0, 4);
+                suffix = fone.substring(4, 8);
+            } else if (settings.useDdd && input.charAt(5) !== '9') {
+                ddd = fone.substring(0, 2);
+                prefix = fone.substring(2, 6);
+                suffix = fone.substring(6, 10);
+            }
+
+            if (settings.useDdd) {
+                ddd = (settings.useDddParenthesis ? '(' : '') + ddd;
+            }
+
+            if (settings.useDdd && prefix.length > 0) {
+                ddd += (settings.useDddParenthesis ? ')' : '') + settings.dddSeparator;
+            }
+
+            if (suffix.length > 0) {
+                prefix += settings.numberSeparator;
+            }
+
+            return ddd + prefix + suffix;
         };
 
-        init();
+        initialize();
 
     };
 
